a5f7177aac318cf07795dd5b19394baa47ea1193
Brayan Alexander Salazar Reyes <90485648+BrayanAlexanderSalazarReyes12@users.noreply.github.com>
2026-09-20 08:24:03 -0500

    Update version test for v1.1.23

--- tests/test_qt_loading.py
+++ tests/test_qt_loading.py
@@ -26,8 +26,8 @@ def _app():
     return app
 
 
-def test_version_is_1_1_22():
-    assert __version__ == "1.1.22"
+def test_version_is_1_1_23():
+    assert __version__ == "1.1.23"
 
 
 def test_startup_splash_reports_percentage():
