64b07d872b7b6efd470853d5849ee0d51e3c39b3
Brayan Alexander Salazar Reyes <90485648+BrayanAlexanderSalazarReyes12@users.noreply.github.com>
2026-09-20 09:01:59 -0500

    [release v1.2.0] Publicar Aegis Auditor 1.2.0
    Publica Aegis Auditor 1.2.0 con el nuevo motor basado en evidencia, deduplicación semántica, matriz endpoint × identidad enriquecida y metadatos de versión alineados.

--- installer/AegisAuditor.iss
+++ installer/AegisAuditor.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Aegis Auditor"
-#define MyAppVersion "1.1.23"
+#define MyAppVersion "1.2.0"
 #define MyAppPublisher "Aegis Auditor Project"
 #define MyAppExeName "AegisAuditor.exe"
 
--- tests/test_qt_loading.py
+++ tests/test_qt_loading.py
@@ -26,8 +26,8 @@ def _app():
     return app
 
 
-def test_version_is_1_1_23():
-    assert __version__ == "1.1.23"
+def test_version_is_1_2_0():
+    assert __version__ == "1.2.0"
 
 
 def test_startup_splash_reports_percentage():
